09ea0a81fe4b2dfc75b6e435f3ac443eb9cb552f
Amel Igoudjil <amel.igoudjil01@gmail.com>
2022-10-28 18:02:07 +0200

    Using the command line program

--- src/main/java/fr/lernejo/umlgrapher/Launcher.java
+++ src/main/java/fr/lernejo/umlgrapher/Launcher.java
@@ -1,31 +1,32 @@
 package fr.lernejo.umlgrapher;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.util.concurrent.Callable;
 
-@Command(name = "launcher", mixinStandardHelpOptions = true, version = "UMLGraph 1.0", description = "UMLGraph")
-public class Launcher implements Callable<Integer> {
+class Launcher implements Callable<Integer> {
 
-    @Option(names = {"-c", "--classes"}, required = true, description = "Select classes to analyse")
+    @Option(names = {"-c", "--classes"},required = true, description = "description de classes")
     private Class<?>[] classes;
 
-    @Option(names = {"-g", "--graph-type"}, defaultValue = "Mermaid", description = "Select graph type")
-    private String graphType;
+    @Option(names = {"-g", "--graph_type"}, description = "type de graph")
+    private final GraphType graphType = GraphType.Mermaid;
 
     @Override
-    public Integer call() throws Exception {
-        UmlGraph graph = new UmlGraph(classes);
-        System.out.println(graph.as(GraphType.valueOf(graphType)));
-        return null;
-    }
+    public Integer call() throws Exception { // your business logic goes here..
 
-    public static void main(String... args){
+        UmlGraph umlGraph = new UmlGraph(classes);
+        String sortie = umlGraph.as(graphType);
+        System.out.println(sortie);
+        return 0;
+    }
+    public static void main(String... args) {
         int exitCode = new CommandLine(new Launcher()).execute(args);
         System.exit(exitCode);
     }
+
+    public void setClasses(Class<?>[] classes) {
+        this.classes = classes;
+    }
 }
-
-
